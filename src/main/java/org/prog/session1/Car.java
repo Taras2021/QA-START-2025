b86c97a3aa9d5b588db05eec77e9cacf965c0f4d
Taras <tpohmursky@gmail.com>
2026-05-18 22:53:20 +0300

    Pohmursky Taras Session 4

---
+++ src/main/java/org/prog/session1/Car.java
@@ -0,0 +1,38 @@
+package org.prog.session1;
+
+//TODO: HW4: add String model and make equals & hashCode work by model and color
+public class Car {
+
+    public String color;
+    public String model;
+
+    public void goTo() {
+        System.out.println(color + " car is going somewhere");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Car) {
+            Car car = (Car) obj;
+            return this.color.equals(car.color)
+            && this.model.equals(car.model);
+        }
+        return false;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return color.hashCode() + model.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Car : " + model + " " + color;
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
